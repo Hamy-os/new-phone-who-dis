fa5bd0c95aec400a81073876fdae6a85ad7f7d66
Kire <nieh.erik@gmail.com>
2021-03-06 10:11:30 -0500

    refactor so profile is created on player load

--- resources/server/sv_twitter.ts
+++ resources/server/sv_twitter.ts
@@ -181,10 +181,11 @@ async function createDefaultProfile(identifier: string): Promise<Profile> {
  * not, create it and return it.
  * @param identifier - player's identifier
  */
-async function getOrCreateProfile(identifier: string): Promise<Profile> {
+export async function getOrCreateProfile(identifier: string): Promise<Profile> {
   const profile = await getProfile(identifier);
   return profile || (await createDefaultProfile(identifier));
 }
+
 
 async function updateProfile(identifier: string, profile: Profile) {
   const { avatar_url, profile_name, bio, location, job } = profile;
@@ -271,8 +272,9 @@ async function doesReportExist(tweetId: number, profileId: number): Promise<bool
 
 onNet(events.TWITTER_GET_OR_CREATE_PROFILE, async () => {
   const _source = getSource();
+  const identifier = getIdentifier(_source);
+  
   try {
-    const identifier = getIdentifier(_source);
     const profile = await getOrCreateProfile(identifier);
     emitNet(events.TWITTER_GET_OR_CREATE_PROFILE_SUCCESS, _source, profile);
   } catch (e) {
